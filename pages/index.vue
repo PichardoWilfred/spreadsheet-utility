e9ccc4be71d90f8c15b89f525bbf52b0e8b6a36f
PichardoWilfred <pichardowilfred@gmail.com>
2024-08-20 09:39:39 -0400

    initialization

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <main class="max-w-[1020px] mx-auto">
+            <section class="p-10">
+                <h1 class="font-bold text-4xl">
+                    All the leaves are brown
+                </h1>
+                <p class="font-" style="font-style: italic;">all the leaves are brown...</p>
+                <div class="mt-5">
+                    <client-only placeholder="loading...">
+                        <QuillEditor :options="options" />
+                    </client-only>
+                </div>
+            </section>
+        </main>
+    </div>
+</template>
+<script setup>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+const options = {
+    debug: 'info',
+        modules: {
+            toolbar: ['bold', 'italic', 'underline']
+        },
+        placeholder: 'Compose an epic...',
+        readOnly: true,
+        theme: 'snow'
+}
+// import Quill from 'quill';
+// Or if you only need the core build
+// import Quill from 'quill/core';
+
+
+</script>
---
+++ plugins/quill.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+const app = createApp()
+app.component('QuillEditor', QuillEditor)
